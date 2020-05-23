174a707ad54535fc6aa169f4e44fe5707ac10f28
adzinets <adzinetskatsiaryna@gmail.com>
2020-05-23 17:36:59 +0300

    Updates

---
+++ precache-manifest.3a8b17abef51c20fb0f3a8d17703539e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e0be94d3542977081e2931bc45b690b",
+    "url": "/CounterTSX/index.html"
+  },
+  {
+    "revision": "148b3329511f3a326628",
+    "url": "/CounterTSX/static/css/main.9cd589df.chunk.css"
+  },
+  {
+    "revision": "c590b6c25236ea1f57fc",
+    "url": "/CounterTSX/static/js/2.cb0895a5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/CounterTSX/static/js/2.cb0895a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "148b3329511f3a326628",
+    "url": "/CounterTSX/static/js/main.13ddf6e1.chunk.js"
+  },
+  {
+    "revision": "cf88866e98eb8ea88e42",
+    "url": "/CounterTSX/static/js/runtime-main.829c5143.js"
+  }
+]);
